b795dc2e941a5a53e9258bbc740ac4fff677cf35
PAVITHRA-D16 <meenapavithra710@gmail.com>
2026-06-17 13:09:35 +0530

    added full adder

--- full_adder_tb.v
+++ full_adder_tb.v
@@ -3,11 +3,15 @@ module tb;
   wire sum,carry;
   full_adder uut(.a(a),.b(b),.cin(cin),.sum(sum),.carry(carry));
   initial begin
-    a=0; b=0;cin=0; #10;
-    a=0; b=1;cin=0; #10;
-    a=1; b=0;cin=0; #10;
-    a=1; b=1;cin=1; #10;
+    a=0; b=0; cin=0; #10;
+    a=0; b=0; cin=1; #10;
+    a=0; b=1; cin=0; #10;
+    a=0; b=1; cin=1; #10;
+    a=1; b=0; cin=0; #10;
+    a=1; b=0; cin=1; #10;
+    a=1; b=1; cin=0; #10;
+    a=1; b=1; cin=1; #10;
     $finish;
   end
   initial $monitor("a=%b b=%b cin=%b sum=%b carry=%b",a,b,cin,sum,carry);
-  endmodule
+   endmodule
